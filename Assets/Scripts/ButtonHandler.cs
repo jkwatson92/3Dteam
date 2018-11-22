a3c202d60f678c46cf5d9e6d738eb9d3740c1957
jkwatson92 <jkwatson@umich.edu>
2018-11-21 22:46:40 -0500

    Started making menu scenes
    Title screen done.

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+    public bool isStart;
+    public bool isQuit;
+    public Text buttonText;
+
+    public void onHover()
+    {
+        buttonText.color = Color.red;
+    }
+
+    public void OnMouseExit()
+    {
+        buttonText.color = Color.black;
+    }
+
+
+    public void OnMouseUp()
+    {
+        if (isStart)
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+
+}
