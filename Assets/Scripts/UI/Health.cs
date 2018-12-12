638b225df2ec76eed239f4b21731b830cb62be72
tfranksSchool <45338840+tfranksSchool@users.noreply.github.com>
2018-12-11 19:30:57 -0500

    Health bar and related things.
    Added a health bar that  changes between sprites when changing health values, but nothing that can actually make use of that yet.
    
    If player health reaches zero, the death scene is loaded.

---
+++ Assets/Scripts/UI/Health.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+	public int numHearts;
+	public Image[] hearts;
+	public Sprite fullHeart;
+	public Sprite emptyHeart;
+
+	// Use this for initialization
+	void Start () {
+		health = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health > numHearts) 
+		{
+			health = numHearts;
+		}
+
+		if (health == 0) {
+			SceneManager.LoadScene("EndScene", LoadSceneMode.Single); //if player dies go to try again screen
+		}
+
+		for (int i = 0; i < hearts.Length; i++) 
+		{
+			if (i < health) 
+			{
+				hearts[i].sprite = fullHeart;
+			}
+			else {
+				hearts[i].sprite = emptyHeart;
+			}
+			if (i < numHearts) 
+			{
+				hearts[i].enabled = true;
+			}
+			else {
+				hearts[i].enabled = false;
+			}
+		}
+	}
+}
