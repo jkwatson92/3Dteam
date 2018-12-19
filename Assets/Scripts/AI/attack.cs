89f1c76e3421aed187cb2c3fa330dbeae3234e52
LindsayPayton <lhilliar@umich.edu>
2018-12-18 20:04:55 -0500

    Revert "Revert "Zachary""

--- Assets/Scripts/AI/attack.cs
+++ Assets/Scripts/AI/attack.cs
@@ -6,84 +6,19 @@ using System;
 //Description: causes game object to head towards another character
 public class attack {
 
-    float force;
-    float velocity;
-    float torque;
+
     GameObject target;
-    Vector3 origin;
-    GameObject entity;
-    float angleDifferenceAllowed;
-    int travelRange;
     UnityEngine.AI.NavMeshAgent agent;
 
 
-    public attack(float force, float velocity, float torque, float angleDifferenceAllowed, GameObject entity, int travelRange, GameObject target, UnityEngine.AI.NavMeshAgent agent)
+    public attack(GameObject entity, GameObject target, UnityEngine.AI.NavMeshAgent agent)
     {
-        this.force = force;
-        this.velocity = velocity;
-        this.torque = torque;
-        this.entity = entity;
-        this.angleDifferenceAllowed = angleDifferenceAllowed;
-        this.travelRange = travelRange;
         this.target = target;
         this.agent = agent;
     }
 
-    public float AdjustAngle(float angle)
-    {
-        while (angle < 0)
-        {
-            angle += 360;
-        }
-        while (angle >= 360)
-        {
-            angle -= 360;
-        }
-        return angle;
-    }
-
-    bool GoClockwise(float angle1, float angle2)
-    {
-        angle1 = AdjustAngle(angle1);
-        angle2 = AdjustAngle(angle2);
-        if (angle1 > angle2)
-        {
-            if (angle1 - angle2 > 180)
-                return true;
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            if (angle2 - angle1 < 180)
-            {
-                return true;
-            }
-            return false;
-        }
-    }
-
-    bool RoundAngle(float angle1, float angle2)
-    {
-        angle1 = AdjustAngle(angle1 + 180f);
-        angle2 = AdjustAngle(angle2);
-        if (angle1 - angle2 < angleDifferenceAllowed && angle1 - angle2 > -angleDifferenceAllowed)
-        {
-            return true;
-        }
-        return false;
-    }
-
     public void update()
     {
-        //point point = new point(torque, angleDifferenceAllowed, target.transform.position, entity);
-        //point.pointTowardsTarget();
-        //if (entity.transform.GetComponent<Rigidbody>().velocity.magnitude < velocity)
-        //{
-        //    entity.transform.GetComponent<Rigidbody>().AddRelativeForce(0, 0, force * Time.deltaTime);
-        //}
         agent.SetDestination(target.transform.position);
     }
 
--- Assets/Scripts/AI/robby.cs
+++ Assets/Scripts/AI/robby.cs
@@ -6,28 +6,58 @@ using UnityEngine;
 public class robby : MonoBehaviour {
     wander wander;
     attack attack;
-    gotopoint gotopoint;
     UnityEngine.AI.NavMeshAgent agent;
-    public float force;
-    public float walkVelocity;
-    public float torque;
-    public float angleDifferenceAllowed;
-    public int travelRange;
     public bool enemyFound;
+    public int health;
+    bool dead;
+    public float rechargetime;
+    float previousattacktime;
 
 	// Use this for initialization
 	void Start () {
+        previousattacktime = 0;
+        dead = false;
         agent = transform.GetComponent<UnityEngine.AI.NavMeshAgent>();
         enemyFound = false;
-        wander = new wander(force, walkVelocity, torque, angleDifferenceAllowed, transform.gameObject, travelRange,agent);
-        attack = new attack(force, walkVelocity, torque, angleDifferenceAllowed, transform.gameObject, travelRange, transform.parent.Find("UserCharacter").gameObject,agent);
-        gotopoint = new gotopoint(force, walkVelocity, torque, angleDifferenceAllowed, transform.gameObject, travelRange, transform.parent.Find("UserCharacter").gameObject.transform.position,agent);
+        wander = new wander(transform.gameObject,agent);
+        attack = new attack(transform.gameObject, transform.parent.parent.Find("UserCharacter").gameObject,agent);
+   }
+
+    public void takeDamage(){
+        health -= 1;
+        if (health <= 0)
+        {
+            agent.isStopped = true;
+            Animator anim = GetComponent<Animator>();
+            anim.Play("fallingback 0");
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(transform.Find("collider").gameObject);
+            dead = true;
+        }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead)
+        {
+            return;
+        }
         if (new vision().userFound(gameObject))
         {
+            if (Time.time - previousattacktime > rechargetime){
+                if (Mathf.Abs(transform.parent.parent.Find("UserCharacter").position.x-transform.position.x)<1){
+                    if (Mathf.Abs(transform.parent.parent.Find("UserCharacter").position.z - transform.position.z) < 1)
+                    {
+                        previousattacktime = Time.time;
+                        transform.parent.parent.Find("UserCharacter").GetComponent<Health>().numHearts -= 1;
+                        Animator anim = GetComponent<Animator>();
+                        anim.Play("attack 0");
+
+                    }
+                }
+            }
             attack.set();
             enemyFound = true;
         }
@@ -37,6 +67,17 @@ public class robby : MonoBehaviour {
             {
                 wander.update();
             }
-         }
+        }
+        if (health <= 0)
+        {
+            agent.isStopped = true;
+            Animator anim = GetComponent<Animator>();
+            anim.Play("fallingback 0");
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(transform.Find("collider").gameObject);
+            dead = true;
+        }
     }
+
 }
